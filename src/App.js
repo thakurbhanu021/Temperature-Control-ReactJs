81174cdceb981f3904512fef4ba22ec439642e1f
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-02-04 19:27:25 +0530

    ui part done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>helo react!</div>
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display cold">10bc</div>
+      </div>
+      <div className="button-container">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
   )
 }
 export default App;
