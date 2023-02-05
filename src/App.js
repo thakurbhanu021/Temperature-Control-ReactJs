61563ef984163862953d9f35536b600d5dfba3ae
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-02-05 16:50:23 +0530

    added logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+
+  const incrementHandler = () => {
+    setTemperatureValue(temperatureValue+1);
+  }
+  const decrementHandler = () => {
+    setTemperatureValue(temperatureValue-1);
+  }
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display cold">10bc</div>
+        <div className="temperature-display cold">{temperatureValue} °C</div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={incrementHandler}>+</button>
       </div>
     </div>
   )
