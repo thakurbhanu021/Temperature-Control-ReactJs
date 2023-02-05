659af7e3765fbec0773fcb67365f93c0c59ac7c2
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-02-05 17:02:27 +0530

    added temperature bg change acc to temp value

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import { useState } from "react";
 
 const App = () => {
   const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
 
   const incrementHandler = () => {
-    setTemperatureValue(temperatureValue+1);
+    if(temperatureValue === 30) return;
+    const newValue = temperatureValue + 1;
+    if(newValue >= 15){
+      setTemperatureColor('hot');
+    }
+    // if (newValue <= 30){
+    // setTemperatureValue(newValue);
+    // } 
+    setTemperatureValue(newValue);
   }
   const decrementHandler = () => {
-    setTemperatureValue(temperatureValue-1);
+    if(temperatureValue === 0) return;
+    const newValue = temperatureValue - 1;
+    if(newValue < 15){
+      setTemperatureColor('cold');
+    }
+    // if (newValue >= 0){
+    // setTemperatureValue(newValue);
+    // }
+    setTemperatureValue(newValue);
   }
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display cold">{temperatureValue} °C</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue} °C</div>
       </div>
       <div className="button-container">
         <button onClick={decrementHandler}>-</button>
